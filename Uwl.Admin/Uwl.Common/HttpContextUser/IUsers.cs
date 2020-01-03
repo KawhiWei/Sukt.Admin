5da898d938f35860ea80b07c5a8dba349e44722a
Aveuy <1585955375@qq.com>
2020-01-03 16:12:15 +0800

    添加一些公共数据

---
+++ Uwl.Admin/Uwl.Common/HttpContextUser/IUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Uwl.Common.HttpContextUser
+{
+    public interface IUsers
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        string Name { get; }
+        Guid Id { get; }
+        bool IsAuthenticated();
+        IEnumerable<Claim> GetClaimsIdentity();
+        List<string> GetClaimValueByType(string ClaimType);
+
+        string GetToken();
+        List<string> GetUserInfoFromToken(string ClaimType);
+    }
+}
---
+++ Uwl.Admin/UwlAPI.Tools/StartupExtension/HttpContextExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Uwl.Common.HttpContextUser;
+
+namespace UwlAPI.Tools.StartupExtension
+{
+    /// <summary>
+    /// 启动服务
+    /// </summary>
+    public static class HttpContextExtension
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddHttpContextExtension(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUsers, HttpContextUserServer>();
+        }
+    }
+}
