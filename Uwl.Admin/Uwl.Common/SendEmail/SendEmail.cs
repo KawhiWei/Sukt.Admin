f3b06b126df673fa0fa287df682a1e534087437e
Avery <1585955375@qq.com>
2020-01-29 13:32:58 +0800

    init

--- Uwl.Admin/Uwl.Common/SendEmail/SendEmail.cs
+++ Uwl.Admin/Uwl.Common/SendEmail/SendEmail.cs
@@ -19,52 +19,26 @@ namespace Uwl.Common.SendEmail
         /// <param name="userName">登录帐号</param>
         /// <param name="pwd">登录密码</param>
         /// <param name="nickName">发件人昵称</param>
-        /// <param name="fromMail">发件人</param>
-        /// <param name="toMail">收件人</param>
+        /// <param name="fromEmail">发件人</param>
+        /// <param name="toEmail">收件人</param>
         /// <param name="subj">主题</param>
         /// <param name="bodys">内容</param>
-        /// <param name="attachments">附件集合，IO流和文件名</param>
-        public static async Task SendMailAsync
-            (string smtpserver, string userName, string pwd, string toMail, 
-             string subj, string bodys, string fromMail = null, string nickName = null, bool enableSsl = false, Dictionary<Stream, string> attachments = null)
+        public static async Task SendMailAsync(string smtpserver, bool enableSsl, string userName, string pwd, string nickName, string fromMail, string toMail, string subj, string bodys)
         {
-            if (string.IsNullOrEmpty(fromMail))
-            {
-                //发件人如果为空则默认登录账户
-                fromMail = userName;
-            }
-            if (string.IsNullOrEmpty(nickName))
-            {
-                //昵称如果为空则默认邮箱前缀
-                nickName = fromMail.Split('@')[0];
-            }
-            MailMessage mailMessage = new MailMessage();
             SmtpClient smtpClient = new SmtpClient();
-            mailMessage.Subject = subj;//主题
-            mailMessage.From = new MailAddress(fromMail, nickName);//发件人和昵称
-            foreach (var item in toMail.Split(','))
-            {
-                mailMessage.To.Add(item);//收件人集合
-            }
-            mailMessage.Body = bodys;//内容
-            mailMessage.BodyEncoding = Encoding.UTF8;//正文编码
-            mailMessage.SubjectEncoding = Encoding.UTF8;//主题编码
-            mailMessage.IsBodyHtml = true;//设置为HTML格式
-            mailMessage.Priority = MailPriority.Normal;//优先级
-            if (attachments != null)
-            {
-                //追加附件文件
-                foreach (var item in attachments)
-                {
-                    mailMessage.Attachments.Add(new Attachment(item.Key, item.Value));
-                }
-            }
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;//指定电子邮件发送方式
             smtpClient.Host = smtpserver;//指定SMTP服务器
             smtpClient.Credentials = new NetworkCredential(userName, pwd);//用户名和密码
             smtpClient.EnableSsl = enableSsl;
-            await smtpClient.SendMailAsync(mailMessage);//发送邮件
-
+            MailAddress fromAddress = new MailAddress(fromMail, nickName);
+            MailAddress toAddress = new MailAddress(toMail);
+            MailMessage mailMessage = new MailMessage(fromAddress, toAddress);
+            mailMessage.Subject = subj;//主题
+            mailMessage.Body = bodys;//内容
+            mailMessage.BodyEncoding = Encoding.Default;//正文编码
+            mailMessage.IsBodyHtml = true;//设置为HTML格式
+            mailMessage.Priority = MailPriority.Normal;//优先级
+            await smtpClient.SendMailAsync(mailMessage);
         }
     }
 }
