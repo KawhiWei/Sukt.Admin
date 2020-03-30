1014228b9fdac2143b405bd0efe929bfb3001428
1585955375@qq.com <1585955375@qq.com>
2020-03-30 21:50:25 +0800

    【add】添加单元测试，测试自动注入

---
+++ Sukt.Core/Sukt.Core.Test/SuktWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Sukt.Core.Shared.SuktAppModules;
+using Sukt.Core.Shared.SuktReflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sukt.Core.Test
+{
+    /// <summary>
+    /// 单元测试工厂
+    /// </summary>
+    /// <typeparam name="TStartup"></typeparam>
+    public class SuktWebApplicationFactory<TStartup>: WebApplicationFactory<TStartup> where TStartup:class
+    {
+        public IServiceCollection IServiceCollection { get; private set; }
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            var bulild = new WebHostBuilder().UseStartup<SuktTestStartup>();
+            return bulild;
+        }
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<IAssemblyFinder, AssemblyFinder>();
+                services.AddSingleton<ISuktAppModuleManager, SuktAppModuleManager>();
+                var provider = services.BuildServiceProvider();
+                var appModuleManager = provider.GetService<ISuktAppModuleManager>();
+                appModuleManager.LoadModules(services);
+                IServiceCollection = services;
+            });
+        }
+
+    }
+}
