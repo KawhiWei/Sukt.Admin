566f4c3904072a0ed47f8fcdc3e10b5e6876eaff
wangzewei <1585955375@qq.com>
2022-02-07 17:41:20 +0800

    feat : 添加k8s健康检查控制器，修改k8s部署文件

---
+++ src/Sukt.Admin.Api/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sukt.Admin.Api.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/api/healthchecks/liveness")]
+        public IActionResult Liveness()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        [Route("/api/healthchecks/readiness")]
+        public IActionResult Readiness()
+        {
+            return Ok();
+        }
+    }
+}
